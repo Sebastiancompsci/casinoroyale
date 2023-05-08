f16b512a70bf78062e0c5b5dba570538792dc83b
thundering redstar <wvongphanith30@gmail.com>
2023-05-08 12:54:51 +0000

    e

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
-# Init.py for Flask App
-
+# General imports
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
+# Route imports
+from routes.index import index
+from routes.home import home
+
 app = Flask(__name__)
+
+app.add_url_rule('/', 'index', view_func=index)
+app.add_url_rule('/home', 'home', view_func=home)
 
 # Start
 def start():
     app.run(debug=True, host='0.0.0.0', port=5000)
-
-@app.route("/home")
-def homepage():
-    return render_template("home.html")
 
 if __name__ == '__main__':
     app.debug = True
