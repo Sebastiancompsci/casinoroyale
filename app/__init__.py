eea1f558c0ed503c0fba2e346e703555621742be
thundering redstar <wvongphanith30@gmail.com>
2023-05-08 12:59:39 +0000

    database work

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,26 @@
 # General imports
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import sqlite3
 
 # Route imports
 from routes.index import index
-from routes.home import home
+from routes.login import login_get, login_post
 
 app = Flask(__name__)
 
+# Routes
 app.add_url_rule('/', 'index', view_func=index)
-app.add_url_rule('/home', 'home', view_func=home)
+app.add_url_rule('/login', 'login_get', view_func=login_get, methods=['GET'])
+app.add_url_rule('/login', 'login_post', view_func=login_post, methods=['POST'])
 
 # Start
 def start():
+    # Initialize database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    # Add c to g
+    app.config['db'] = c
     app.run(debug=True, host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
